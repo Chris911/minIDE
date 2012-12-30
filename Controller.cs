9e6ad729709463a4055d0a548b108a573c2e99c5
Chris <christophe.naud.dulude@gmail.com>
2012-12-30 17:45:08 -0500

    Initial Commit

---
+++ Controller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace minIDE
+{
+    class Controller
+    {
+        // Should be placed in configuration file
+        String ideOneUser     = "";
+        String ideOnePassword = "";
+
+        public String sendCodeToServer(String sourceCode, int language)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            Ideone_ServiceService client = new Ideone_ServiceService();
+            Object[] ret = client.createSubmission(ideOneUser, ideOnePassword, sourceCode, language, null, true, true);
+            
+            foreach (object o in ret)
+            {
+                if (o is XmlElement)
+                {
+                    XmlNodeList x = ((XmlElement)o).ChildNodes;
+                    result.Add(x.Item(0).InnerText, x.Item(1).InnerText);
+                }
+            }
+
+            Object[] retDet = client.getSubmissionDetails(ideOneUser, ideOnePassword, result["link"],
+                false, true, true, true, true);
+
+            foreach (object o in retDet)
+            {
+                if (o is XmlElement)
+                {
+                    XmlNodeList x = ((XmlElement)o).ChildNodes;
+                    if(!result.ContainsKey(x.Item(0).InnerText))
+                        result.Add(x.Item(0).InnerText, x.Item(1).InnerText);
+                }
+            }
+
+            return buildOutputMessage(result);
+        }
+
+        // Should be moved out of this class
+        private String buildOutputMessage(Dictionary<string, string> result)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (KeyValuePair<string, string> kvp in result)
+            {
+                builder.Append(kvp.Key + " : " + kvp.Value + "\n");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AvalonDock.Layout;
+using AvalonDock.Layout.Serialization;
+
+namespace minIDE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Controller controller;
+
+        public MainWindow()
+        {
+            //InitializeComponent();
+            //var firstDocumentPane = dockingManager.Layout.;
+            
+            //Init Controller
+            controller = new Controller(); 
+        }
+
+        private void compileButton_Click(object sender, RoutedEventArgs e)
+        {
+            String codeText = codeTextBox1.Text;
+            String outputText = controller.sendCodeToServer(codeText, 55);
+            outputTextBox1.Text = outputText;
+        }
+
+
+
+    }
+}
